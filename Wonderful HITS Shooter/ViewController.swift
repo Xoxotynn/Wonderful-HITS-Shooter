1714be40ee8f6efc1fe1a9e722a378419e568174
Edward <tamerlan701@gmail.com>
2021-12-19 21:08:27 +0700

    Initial Commit

---
+++ Wonderful HITS Shooter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wonderful HITS Shooter
+//
+//  Created by Эдуард Логинов on 19.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
